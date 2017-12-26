ab7e6e9d81ea71bb8b0acb9861abcd125bd2baea
Prateek Kanhere <kanherepratik@gmail.com>
2017-12-27 00:43:00 +0530

    Update homepage.js

--- tests/homepage.js
+++ tests/homepage.js
@@ -5,12 +5,7 @@ module.exports = {
       .url('http://www.google.com')
       .waitForElementVisible('body', 1000)
       .assert.title('Google')
-      .assert.visible('input[type=text]')
-      .setValue('input[type=text]', 'rembrandt van rijn')
-      .waitForElementVisible('input[name=btnK]', 3000)
-      .click('input[name=btnK]')
-      .pause(1000)
-      .saveScreenshot('screenshots/google.png')
+      .saveScreenshot('google.png')
       .end()
   }
 };
